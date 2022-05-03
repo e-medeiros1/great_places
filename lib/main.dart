5d12c0e248194434e2c492cfa2b4f916380537c9
Medeiros <hsymetry@gmail.com>
2022-05-03 18:57:08 -0300

    Set theme and main home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:great_places/screens/places_list_screen.dart';
 
 void main(List<String> args) {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      
+      title: 'Meus lugares',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          onPrimary: Colors.black,
+          onSecondary: Colors.white,
+        ),
+      ),
+      home: const PlacesListScreen(),
     );
   }
 }

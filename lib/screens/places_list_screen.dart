8cc1e64c060f2658ef20c743b8fba03c04e1c7a6
Medeiros <hsymetry@gmail.com>
2022-05-03 18:54:30 -0300

    Create first page

---
+++ lib/screens/places_list_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class PlacesListScreen extends StatelessWidget {
+  const PlacesListScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meus lugares'),
+        centerTitle: true,
+      ),
+      body: const CircularProgressIndicator(),
+    );
+  }
+}

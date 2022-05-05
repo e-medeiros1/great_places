2559a653306867996dfce443dd9e9afd73547fa7
Medeiros <hsymetry@gmail.com>
2022-05-05 18:27:06 -0300

    Visual changes on placeForm and main Theme

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Meus lugares',
       theme: ThemeData(
+        iconTheme:
+            IconThemeData(color: Theme.of(context).colorScheme.onSecondary),
         colorScheme: ColorScheme.fromSwatch().copyWith(
           onPrimary: Colors.black,
           onSecondary: Colors.white,

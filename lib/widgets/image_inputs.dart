8e2ae1fd0c60762344e3e0786a1c8eda4214aba3
Medeiros <hsymetry@gmail.com>
2022-05-12 10:47:27 -0300

    Add Image input component

---
+++ lib/widgets/image_inputs.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ImageInputs extends StatefulWidget {
+  const ImageInputs({Key? key}) : super(key: key);
+
+  @override
+  State<ImageInputs> createState() => _ImageInputsState();
+}
+
+class _ImageInputsState extends State<ImageInputs> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: 220,
+          height: 220,
+          decoration:
+              BoxDecoration(border: Border.all(width: 1, color: Colors.grey)),
+        ),
+        TextButton.icon(
+            onPressed: () {},
+            icon: Icon(
+              Icons.camera_alt,
+              color: Colors.grey.shade800,
+            ),
+            label: Text(
+              'Tirar foto',
+              style: TextStyle(color: Colors.grey.shade600),
+            ))
+      ],
+    );
+  }
+}

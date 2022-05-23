2095aa34b291939463508b4e1da810decba33fe1
Medeiros <hsymetry@gmail.com>
2022-05-23 10:45:13 -0300

    Adds changeNotifierProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:great_places/others/app_routes.dart';
+import 'package:great_places/providers/great_places.dart';
 import 'package:great_places/screens/place_form_screen.dart';
 import 'package:great_places/screens/places_list_screen.dart';
+import 'package:provider/provider.dart';
 
 void main(List<String> args) {
   runApp(const MyApp());
@@ -12,17 +14,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Meus lugares',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch()
-            .copyWith(primary: Colors.blueGrey, secondary: Colors.white),
+    return ChangeNotifierProvider(
+      create: (ctx) => GreatPlaces(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Meus lugares',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch()
+              .copyWith(primary: Colors.blueGrey, secondary: Colors.white),
+        ),
+        home: const PlacesListScreen(),
+        routes: {
+          AppRoutes.PLACE_FORM: (ctx) => const PlaceFormScreen(),
+        },
       ),
-      home: const PlacesListScreen(),
-      routes: {
-        AppRoutes.PLACE_FORM: (ctx) => const PlaceFormScreen(),
-      },
     );
   }
 }

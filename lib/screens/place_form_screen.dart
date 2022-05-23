b46ad218d1b9c5e6d39d6bf2886911fbc3179ac8
Medeiros <hsymetry@gmail.com>
2022-05-23 10:46:28 -0300

    add method to submit form

--- lib/screens/place_form_screen.dart
+++ lib/screens/place_form_screen.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:great_places/others/app_routes.dart';
+import 'package:great_places/providers/great_places.dart';
 import 'package:great_places/widgets/image_inputs.dart';
+import 'package:provider/provider.dart';
 
 class PlaceFormScreen extends StatefulWidget {
   const PlaceFormScreen({Key? key}) : super(key: key);
@@ -11,8 +16,22 @@ class PlaceFormScreen extends StatefulWidget {
 //TO-DO: Setar a cor tema e separar label do textfield entre normal e onFocused
 class _PlaceFormScreenState extends State<PlaceFormScreen> {
   final _titleController = TextEditingController();
+  File? _pickedImage;
 
-  void _submitForm() {}
+  void _selectImage(File pickedImage) {
+    _pickedImage = pickedImage;
+  }
+
+  void _submitForm() {
+    if (_titleController.text.isEmpty || _pickedImage == null) {
+      return;
+    }
+
+    Provider.of<GreatPlaces>(context, listen: false)
+        .addPlaces(_titleController.text, _pickedImage!);
+
+    Navigator.of(context).pop();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +51,7 @@ class _PlaceFormScreenState extends State<PlaceFormScreen> {
           body: Column(
             children: [
               const SizedBox(height: 20),
-              const ImageInputs(),
+              ImageInputs(onSelectImage: this._selectImage),
               Expanded(
                 child: Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 15.0),

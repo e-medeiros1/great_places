b645f9c151bae80d4a825bb684a79edf10fdd39e
Medeiros <hsymetry@gmail.com>
2022-05-26 11:06:53 -0300

    Add LocationInput component

---
+++ lib/widgets/location_inputs.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:great_places/others/location_util.dart';
+import 'package:location/location.dart';
+
+class LocationInputs extends StatefulWidget {
+  const LocationInputs({Key? key}) : super(key: key);
+
+  @override
+  State<LocationInputs> createState() => _LocationInputsState();
+}
+
+class _LocationInputsState extends State<LocationInputs> {
+  String? _previewImageUrl;
+  Future<void> _getCurrentUserLocation() async {
+    final locData = await Location().getLocation();
+    final staticMapImageUrl = LocationUtil.GenerateLocationPreviewImage(
+      latitude: locData.latitude!,
+      longitude: locData.longitude!,
+    );
+    setState(() {
+      _previewImageUrl = staticMapImageUrl;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Column(
+      children: [
+        Container(
+          height: size.height * 0.27,
+          width: size.width * 0.9,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            border: Border.all(width: 2, color: Colors.grey.shade700),
+            borderRadius: BorderRadius.circular(
+              20,
+            ),
+          ),
+          child: _previewImageUrl == null
+              ? Text(
+                  'Preview indisponível',
+                  style: TextStyle(color: Colors.grey.shade700),
+                )
+              : ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Image.network(
+                    _previewImageUrl!,
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                  ),
+                ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextButton.icon(
+              onPressed: _getCurrentUserLocation,
+              icon: Icon(
+                Icons.location_on,
+                color: Colors.grey.shade800,
+              ),
+              label: Text(
+                'Localização atual',
+                style: TextStyle(color: Colors.grey.shade600),
+              ),
+            ),
+            TextButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.map, color: Colors.grey.shade800),
+              label: Text(
+                'Selecione no mapa',
+                style: TextStyle(color: Colors.grey.shade600),
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}

81dd91210453b240cd45e5ef790260f5af4148c0
Medeiros <hsymetry@gmail.com>
2022-05-30 10:39:03 -0300

    Adds method to select and mark positions on map

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -4,32 +4,75 @@ import 'package:great_places/models/places.dart';
 
 class MapScreen extends StatefulWidget {
   final PlacesLocation initialLocation;
+  final bool isReadOnly;
 
-  MapScreen(
-      {Key? key,
-      this.initialLocation = const PlacesLocation(
-          latitude: -23.586479384589293, longitude: -46.68211058908439)})
-      : super(key: key);
+  MapScreen({
+    Key? key,
+    this.isReadOnly = false,
+    //Posição setada para o escritório do google em SP
+    this.initialLocation = const PlacesLocation(
+        latitude: -23.586479384589293, longitude: -46.68211058908439),
+  }) : super(key: key);
 
   @override
   State<MapScreen> createState() => _MapScreenState();
 }
 
 class _MapScreenState extends State<MapScreen> {
+  LatLng? _pickedPosition;
+
+  void _selectPosition(LatLng position) {
+    setState(() {
+      _pickedPosition = position;
+    });
+    print(_pickedPosition);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Selecione...'),
+        elevation: 1,
+        iconTheme:
+            IconThemeData(color: Theme.of(context).colorScheme.secondary),
+        title: Text(
+          'Selecione...',
+          style: TextStyle(color: Theme.of(context).colorScheme.secondary),
+        ),
+        actions: [
+          if (!widget.isReadOnly)
+            IconButton(
+              color: Theme.of(context).colorScheme.secondary,
+              onPressed: _pickedPosition == null
+                  ? null
+                  : () {
+                      Navigator.of(context).pop(_pickedPosition);
+                    },
+              icon: const Icon(Icons.check),
+            ),
+        ],
       ),
       body: GoogleMap(
+        zoomGesturesEnabled: true,
         initialCameraPosition: CameraPosition(
+          
           target: LatLng(
             widget.initialLocation.latitude,
             widget.initialLocation.longitude,
           ),
-          zoom: 14,
+          zoom: 13,
         ),
+        onTap: widget.isReadOnly ? null : _selectPosition,
+        //Set é uma lista de elementos que não aceita repetição
+        //Se a posição não estiver selecionada vai retornar null
+        markers: _pickedPosition == null
+            ? Set()
+            : {
+                Marker(
+                  markerId: const MarkerId('p1'),
+                  position: _pickedPosition!,
+                ),
+              },
       ),
     );
   }
